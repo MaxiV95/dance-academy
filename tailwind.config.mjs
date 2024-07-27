0caccc7827a6837c0156cb1a76100132931fb922
Maximiliano Van Megroot <maximilianovanmegroot@gmail.com>
2024-07-27 11:03:24 -0300

    schedules

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: "#1a1a1a",
+				red_d: "#b30000",
+				gold_d: "#d4af37",
+				silver_d: "#c0c0c0",
+
+				light: "#f5f5f5",
+				red_l: "#c8102e",
+				gold_l: "#f9c74f",
+				silver_l: "#b0b0b0",
+			},
+		},
 	},
 	plugins: [],
-}
+};
